1a45f6648652c8f95b400733803257d0aee6de6c
Nate SmallWater <smallwaterdev@gmail.com>
2018-07-12 19:48:12 +0800

    seo

--- smallData/sitemap_generator.js
+++ smallData/sitemap_generator.js
@@ -11,11 +11,9 @@ const filename = "./seo/sitemap/sitemap.xml";
 const url_origin = "http://www.javferry.com"
 function initSiteMap(filename, callback){
     fs.writeFile(filename, `<?xml version="1.0" encoding="UTF-8"?>
-<urlset
-    xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
-    xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-    xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9
-        http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">`, callback);
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" 
+      xmlns:image="http://www.google.com/schemas/sitemap-image/1.1" 
+      xmlns:video="http://www.google.com/schemas/sitemap-video/1.1">`, callback);
 }
 
 function addInitPage(filenmae, callback){
