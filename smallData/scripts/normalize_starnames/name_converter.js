40902104e42bf95f5f377660bc9148c77d62a28a
Nate SmallWater <smallwaterdev@gmail.com>
2018-07-13 00:35:30 +0800

    starname

---
+++ smallData/scripts/normalize_starnames/name_converter.js
@@ -0,0 +1,74 @@
+const name_converter = {};
+name_converter['ai-1989'] = 'ai';
+name_converter['av-9898-shinoda-ayumi'] = 'shinoda-ayumi';
+name_converter['azusa-kirihara-(azusa-ito)'] = 'azusa-kirihara';
+name_converter['ayumu-sena-(aiko-hirose)'] = 'ayumu-sena';
+name_converter['azusa-kirihara-(azusa-ito)'] = 'azusa-kirihara';
+name_converter['eri--sasaki'] = 'eri-sasaki';
+name_converter['fujikawa-reina-(kuramochi-akane)'] = 'fujikawa-reina';
+name_converter['hasumi--kawaguchi'] = 'hasumi-kawaguchi'
+name_converter['hitomi-(hitomi-tanaka)'] = 'hitomi-tanaka';
+name_converter['i-akiyoshi-kanon'] = 'akiyoshi-kanon';
+name_converter['i-haru-marin'] = 'haru-marin';
+name_converter['i-kitakami-momo'] = 'kitakami-momo';
+name_converter['i-nishitani-moe'] = 'nishitani-moe';
+name_converter['jiyuu--kanade'] = 'jiyuu--kanade';
+name_converter['kaoru-natsuki'] = 'kaoru-natsuki';
+name_converter['kokone-mizutani-(rio-fujisaki)'] = 'kokone-mizutani';
+name_converter['kudo--mami'] = 'kudo-mami';
+name_converter['madoka-ogawa)'] = 'madoka-ogawa';
+name_converter['masumi-takasaka)'] ='masumi-takasaka';
+name_converter['meguri-(megu-fujiura)'] = 'megu-fujiura';
+name_converter['mia--shizuku'] = 'mia-shizuku';
+name_converter['miki-iwashita)'] = 'miki-iwashita';
+name_converter['mizuki--sakurai'] = 'mizuki-sakurai';
+name_converter['naho-kuroki)'] = 'naho-kuroki';
+name_converter['natsuki-ando)'] = 'natsuki-ando';
+name_converter['nonoka-kaede-(yume-asakura)'] = 'nonoka-kaede';
+name_converter['yuuka--aoba'] ='yuuka-aoba';
+name_converter['yu-kawakami-(shizuku-morino)'] = 'yu-kawakami';
+//otsu-to-sakino
+//paris-hilton-hotel
+//rain-sound-wakana
+//
+//natsuki-yokoyama.-yui-kawagoe.-miori-hara.-rena-aoi
+//niiyama-sa-ya
+//nori-miyami-also
+//ogawa-momoka-aika-mirei-aoyama-hana-nikaidou-yuri
+//
+//nitr-034
+//nana-yuuki-miu
+//nao-masaki-(momo-kaede
+//mizuki-yu-bud
+//morikawa-hina-q
+//miina-kannomiina-kanno
+//miki-sunohara-miyu-saito
+//miss-star-luka
+//mitsuki-catherine-yokota
+//mitsuko-shima-ue
+//miura-starry-sky
+//mari-aoyama-nana
+//mari-sasaki-rumi-sugimoto
+//matsuoka-sand-feather
+//maezaki-flower-season
+//kuroki-ikumi-asahi-nana-takashiro-amina
+//asa-iku-sana
+//kiara-lord-melanie-sienna-day
+//koyuki-hosono-kozu
+//kira--av-sikou
+//japanese-av-model
+//julia-ann-lisa-ann-oil
+//jun-airi-sato
+//kagney-linn-karter
+//kamihata-ichika-misaki-yuu-miyake-mika-aizawa-yurina-hidaka-yua-tsukimoto-ai-mochizuki-sakura-sano-aoi
+//kanda-rina-no-hana
+//hijiri-greens-arisa
+//hikari-rin-makiko
+//hikari☆
+//eri-rin-katagiri
+//girl
+//haduki-naho-videos
+//beautiful
+//aika-mirei-ichikawa-tsubasa-hoshina-ai-ooto-wakaha-sazanami-rino-yuuri-maina
+//haruka-ohsawa，nana-kaisaki，mayumi，riku-sena
+module.exports.name_converter = name_converter;
